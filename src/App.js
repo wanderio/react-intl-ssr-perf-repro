171812e03dc2dc149104258f3b683679f2985da9
Luca Barone <baro.luc@gmail.com>
2019-10-08 15:58:25 +0200

    add translations and full-icu package

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
 const {html} = require('htm/react')
+const {IntlProvider, FormattedMessage, useIntl} = require('react-intl')
+
+const locale = 'it'
+const messages = ['a', 'b', 'c', 'd', 'e', 'f']
+  .map(l => [l, l.toUpperCase()])
+  .reduce((acc, [lower, upper]) => ({...acc, [lower]: upper}), {})
+
+console.log({messages})
+
+const Home = () => {
+  const {formatMessage} = useIntl()
+
+  return html`
+    <div>
+      <h1>Hello, React Intl</h1>
+      <h2>
+        <${FormattedMessage} id="a" defaultMessage="FOOO" />
+      </h2>
+
+      <h2>
+        <${FormattedMessage} id="b" defaultMessage="FOOO" />
+      </h2>
+      <h2>
+        <${FormattedMessage} id="c" defaultMessage="FOOO" />
+      </h2>
+      <h2>
+        ${formatMessage({id: 'd', defaultMessage: 'FOOO'})}
+      </h2>
+      <p>Lorem, ipsum dolor.</p>
+      <h2>
+        <${FormattedMessage} id="e" defaultMessage="FOOO" />
+      </h2>
+      <h2>
+        <${FormattedMessage} id="f" defaultMessage="FOOO" />
+      </h2>
+    </div>
+  `
+}
 
 const App = () => {
   return html`
-    <h1>Hello, React Intl</h1>
+    <${IntlProvider} locale=${locale} messages=${messages}>
+      <${Home} />
+    </${IntlProvider}>
   `
 }
 
